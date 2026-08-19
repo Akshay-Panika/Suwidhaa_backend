3b2ec374e2f46acfb789504b62a4363183138654
akshe <akshey.panika@gmail.com>
2026-08-19 12:53:06 +0530

    Fix database configuration dependency

--- suwidhaa_backend/urls.py
+++ suwidhaa_backend/urls.py
@@ -17,4 +17,8 @@ urlpatterns = [
         "api/v1/school/",
         include("school.student.urls"),
     ),
+    path(
+         "api/v1/school/",
+         include("school.teacher.urls"),
+    ),
 ]
