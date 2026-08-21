e54e8f5c2193f62aaa74298d12ce93dc23216d37
akshe <akshey.panika@gmail.com>
2026-08-21 13:25:26 +0530

    Fix database configuration dependency

--- suwidhaa_backend/settings.py
+++ suwidhaa_backend/settings.py
@@ -27,6 +27,7 @@ cloudinary.config(
 )
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
