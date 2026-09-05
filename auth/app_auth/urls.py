4f10280689822f39d0add724c85da282265ae806
akshe <akshey.panika@gmail.com>
2026-09-05 13:41:23 +0530

    update APIs

--- auth/app_auth/urls.py
+++ auth/app_auth/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/register/', views.RegisterView.as_view(), name='register'),
-    path('api/login/', views.LoginView.as_view(), name='login'),
-    path('api/users/', views.UserListView.as_view(), name='user-list'),
-    path('api/users/<int:user_id>/delete/', views.UserDeleteView.as_view(), name='user-delete'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('users/', views.UserListView.as_view(), name='user-list'),
+    path('users/<int:user_id>/delete/', views.UserDeleteView.as_view(), name='user-delete'),
 ]
