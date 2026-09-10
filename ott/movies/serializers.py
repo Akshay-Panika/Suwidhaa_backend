37773623c937b66c472cd02f8ff9dd5937485931
akshe <akshey.panika@gmail.com>
2026-09-10 15:18:10 +0530

    Add movies migrations

--- ott/movies/serializers.py
+++ ott/movies/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 from .models import Movie
 
+
 class MovieSerializer(serializers.ModelSerializer):
     content_type = serializers.CharField(read_only=True)
+    thumbnail_horizontal = serializers.SerializerMethodField()
+    thumbnail_vertical = serializers.SerializerMethodField()
 
     class Meta:
         model = Movie
         fields = '__all__'
         read_only_fields = ('id', 'content_type', 'created_at', 'updated_at')
+
+    def get_thumbnail_horizontal(self, obj):
+        if obj.thumbnail_horizontal:
+            return obj.thumbnail_horizontal.url
+        return None
+
+    def get_thumbnail_vertical(self, obj):
+        if obj.thumbnail_vertical:
+            return obj.thumbnail_vertical.url
+        return None
